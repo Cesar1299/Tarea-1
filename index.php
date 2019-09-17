3862ddbc462fe292fdc8bd3b7e744613fdd02df8
“Cesar1299” <20175181@itla.edu.do>
2019-09-17 15:52:56 -0400

    “Presentacion”

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<title>Tarea 1</title>
+</head>
+<body>
+<?php
+echo "<h1>Me llamó César Antonio González </h1><br>
+
+<p> Mi experiencia en el área de software es variada ya que visto y practicado muchos lenguajes como Java, C#, 
+C++, app moviles android, entre otras. En proceso llevo un conjunto de planes y app que ayudaran de una u otra
+forma a cubrir necesidades para esto investigo y aprendo de todo lo que puede facilitar la internet y compañeros del área.
+</p></br><br>Hobbies: Bastkeball, escuchar música, ver Netflix, compartir con mis amistades, crear apps. </br>";
+
+?>
+</body>
+</html>
